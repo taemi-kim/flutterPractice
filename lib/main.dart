a174152665facd717385f56f0fadf4dd3d103946
tkim105 <tkim105@jh.edu>
2023-08-19 08:49:12 +0900

    created four different widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,14 @@ class  MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) { //여기까지가 메인페이지 만드는 문법
     return MaterialApp(
-      home: Text('안녕') //위젯 포맷
+      home: Center( //내 자식 위젯의 기준점을 센터로 정해줌
+        child: Container( width: 50, height: 50, color: Colors.blue),
+      )
+      //글자위젯 Text('안녕')
+      //이미지위젯 Image.asset('memoji.png')
+      //아이콘위젯 Icon(Icons.star)
+      //박스위젯 플러트 크기 단위: LP -> 50LP = 1.2cm
+      //Container( width: 50, height: 50, color: Colors.blue )
     );
 
   }
